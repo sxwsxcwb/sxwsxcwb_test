5a3f7938a55af7dcac4c6955d5c2d1c4e6eaabae
sxwsxcwb <845102659@qq.com>
2014-12-10 23:44:48 +0800

    Update Test.java
    更新 打印这个工具很强大

--- src/com/sxwsxcwb/Test.java
+++ src/com/sxwsxcwb/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("����github!");
+		System.out.println("这个工具不错,很强大");
 	}
 
 }
