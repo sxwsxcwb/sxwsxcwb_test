1274123edcf471edf23a9464f95737a9707d8b01
sxwsxcwb <845102659@qq.com>
2014-12-10 23:56:47 +0800

    这是本地分支

--- src/com/sxwsxcwb/Test.java
+++ src/com/sxwsxcwb/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for(int i = 0; i<100; i++){
-			System.out.println("这个工具不错,很强大");
+			System.out.println("这个是本地分支");
 		}
 		
 	}
